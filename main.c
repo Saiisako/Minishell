45791fc7bdc8082430362f55a4306f76a1df0561
Sacha Kock <kock.sacha@gmail.com>
2025-03-10 20:16:47 +0100

    added builtin(1/7) = env + normed

--- main.c
+++ main.c
@@ -6,11 +6,26 @@
 /*   By: skock <skock@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/10 10:43:47 by skock             #+#    #+#             */
-/*   Updated: 2025/03/10 20:07:52 by skock            ###   ########.fr       */
+/*   Updated: 2025/03/10 20:15:28 by skock            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
+
+void	print_env(t_ms *minishell)
+{
+	t_env	*tmp;
+
+	tmp = minishell->env_lst;
+	while (tmp)
+	{
+		printf("%s", tmp->key);
+		printf("=");
+		printf("%s\n", tmp->value);
+		tmp = tmp->next;
+	}
+	return ;
+}
 
 int	main(int ac, char **av, char **envp)
 {
@@ -29,6 +44,8 @@ int	main(int ac, char **av, char **envp)
 			input = readline("minishell ➜ $");
 			if (input && *input)
 				add_history(input);
+			if (!ft_strcmp(input, "env"))
+				print_env(minishell);
 			printf("%s\n", input);
 			free(input);
 		}
